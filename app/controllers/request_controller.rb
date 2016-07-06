58b3e9704d610d54eec68a2d17980b47ec0771ab
e1michi <kaz@takahashi.com>
2016-07-06 06:48:32 +0000

    kaz

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -23,8 +23,7 @@ class RequestController < ApplicationController
       unless model.content.contentType == 1
         next
       end
-      text = 'hello'
-      post(model.content.from, text)
+      post(model.content.from, model.content.text)
     end
 
     # container = result[:content]

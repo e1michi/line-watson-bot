96a1686384fad0841e3e9a756e4413281f36cbc9
e1michi <kaz@takahashi.com>
2017-03-03 17:34:38 +0000

    k

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -119,12 +119,11 @@ class LineBotController < ApplicationController
       columns = []
       result['rest'].each do | item |
         col = {}
-        col[:thumbnailImageUrl] = item['image_url']['shop_image1']
-        col[:title] = item['name']
+        col[:thumbnailImageUrl] = item['image_url']['shop_image1'] if item['image_url']['shop_image1'].length > 0
+        col[:title] = item['name'] if item['name'].length > 0
+        col[:text] = '説明は詳細にて確認してください。'
         if item['pr']['pr_short'].length > 0
           col[:text] = item['pr']['pr_short'][0,60]
-        else
-          col[:text] = '説明は詳細にて確認してください。'
         end
         action = {}
         action[:type] = 'uri'
